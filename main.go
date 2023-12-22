dc6e5a00902c63282d975ac855e20fe1f811f579
trytillyoumakeit <trytillyoumakeit@gmail.com>
2023-12-22 18:42:02 +0700

    server api

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"simplebank/api"
+	db "simplebank/db/repository"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://td:secret@localhost:5432/simplebank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+	defer conn.Close()
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+
+}
