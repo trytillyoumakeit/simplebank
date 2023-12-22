e21d904d86dafce6267ed35eda1fcd74d38a8cb7
trytillyoumakeit <trytillyoumakeit@gmail.com>
2023-12-22 19:22:20 +0700

    update main test

--- internal/repository/main_test.go
+++ internal/repository/main_test.go
@@ -4,24 +4,22 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"simplebank/util"
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://td:secret@localhost:5432/simplebank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
 
-	var err error
-
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
